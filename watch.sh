8921a639654c65220b14e6f6183c01f587d4437c
rnowm <arnau@arnaumarch.com>
2015-03-24 03:09:45 -0700

    updated font

--- watch.sh
+++ watch.sh
@@ -1,9 +1,11 @@
-git pull
-grunt
-git commit -am"updated font"
-git push
-git checkout gh-pages
-git pull origin master
-git push origin gh-pages
-git checkout master
-
+RES=`git pull | wc -l`
+if [ "$RES" -gt 1 ]
+then
+	grunt
+	git commit -am"updated font"
+	git push
+	git checkout gh-pages
+	git pull origin master
+	git push origin gh-pages
+	git checkout master
+fi
