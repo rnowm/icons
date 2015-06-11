8061f551f3e1c8ad259edc09f3422af67eea3c5c
hsunglee <hyunsung.leelee@telefonica.com>
2015-06-11 11:25:28 +0200

    menu

--- csb-test/js/scripts.js
+++ csb-test/js/scripts.js
@@ -38,84 +38,12 @@ function hasScrolled() {
     lastScrollTop = st;
 }
 
-
-// ***** SLIDER *****
-$(document).ready(function(){
-
-    /*
-     * Variables
-     */
-
-    var slides;
-
-    //Crea un objeto con informaciÃ³n de los slides
-    slides = $('#product-slider .product-list > .box');
-
-
-    /*
-     * Funciones
-     */
-    
-    //Desplaza el contenedor hacia la direcciÃ³n definida
-    //@direction = [left,right]
-    function sliderScroll(direction){
-
-       //Calcula la posiciÃ³n actual del contenedor
-       position = $('#product-slider').scrollLeft();
-
-       //Calcula la anchura total menos el Ãºltimo slide.
-       //Se usa para calcular cuando el scroll llega al final.
-       totalWidth = (slides.length * slides[0].offsetWidth) - slides[0].offsetWidth
-
-       //Se comprueba la variable direction para hacer el scroll hacia izquierda o derecha
-       switch (direction) {
-            case 'right': //Derecha
-                if (position+slides[0].offsetWidth > totalWidth){ //Si la siguiente posiciÃ³n se sale del contenedor, vuelve al principio.
-                    $('#slider:not(:animated)').animate({scrollLeft:0},1000);
-                } else { //Si no es el final, suma a la posiciÃ³n actual la anchura del slide.
-                    $('#slider:not(:animated)').animate({scrollLeft:position+slides[0].offsetWidth},1000);
-                }
-                break;
-
-            case 'left': //Izquierda
-                if (position-slides[0].offsetWidth < 0){ //Si la siguiente posiciÃ³n se sale del contenedor, vuelve al final.
-                    $('#slider:not(:animated)').animate({scrollLeft:totalWidth},1000);
-                } else { //Si no es el final, resta a la posiciÃ³n actual la anchura del slide.
-                    $('#slider:not(:animated)').animate({scrollLeft:position-slides[0].offsetWidth},1000);
-                }
-                break;
-        }
-
-   }
-   /*
-    * CÃ³digo
-    */
-
-    //Asigna el ancho total de los slides al contenedor
-    //La anchura total se obtiene multiplicando la medida de un slide por el nÃºmero de slides)
-    $('#slider .slidesContainer').css('width',slides[0].offsetWidth * slides.length);
-
-
-    //Click en el botÃ³n "next"
-    $('.next').click(function(){
-        sliderScroll('right'); //Mueve el scroll a la derecha
-        initTimer(); //Vuelve a activar el temporizador
-        return false;
-    });
-
-    //Click en el botÃ³n "prev"
-    $('.prev').click(function(){
-        sliderScroll('left'); //Mueve el scroll a la izquierda
-        initTimer(); //Vuelve a activar el temporizador
-        return false;
-    });
-
-
-});
-
-
 // ***** NAV *****
 $(".toggle-nav").on('click', function (e) {
     $("#main-menu").toggleClass("show");
     e.preventDefault();
-})
+});
+
+$(".menu-bg").on('click', function (e) {
+    $("#main-manu").removeClass("show");
+});
